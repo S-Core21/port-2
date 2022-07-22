27a0d88469b1c9241982f4ef5bac6b32811b7ab4
Chukwuebuka Ogbonna <ogbonnashedrach8@gmail.com>
2022-07-22 20:39:39 +0100

    progress

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let menuClose = document.querySelector('.close')
 let menuOpen = document.querySelector('.menu')
 let navBar = document.querySelector('nav')
+let querry = window.matchMedia('max-width: 420px')
 
+if(querry.matches){
 navBar.style.cssText = 'transform: translateX(260px);'
+}
 
 menuOpen.addEventListener('click', ()=> {
     navBar.style.cssText = 'transform: translateX(0px); transition: 1s linear 0.3s'
